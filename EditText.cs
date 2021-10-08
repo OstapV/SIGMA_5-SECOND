9ed27c9e02a418fee671989eacb5ef8112b1386e
OstapV <92176833+OstapV@users.noreply.github.com>
2021-10-08 20:23:52 +0300

    Add files via upload

---
+++ EditText.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+
+namespace SIGMA_5_SECOND_
+{
+    class EditText
+    {
+        private string[] content;
+
+        public string[] Content
+        {
+            get { return content; }
+            set { content = value; }
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < Content.Length; i++)
+            {
+                Console.WriteLine(Content[i]);
+            }
+        }
+        public void ReadText(string path)
+        {
+            StreamReader sr = new StreamReader(path);
+
+            string text = sr.ReadToEnd().Replace("\r", String.Empty);
+
+            Content = text.Split('\n');
+        }
+
+        public int GetHashSum()
+        {
+            int hash = 0;
+
+            for (int i = 0; i < Content.Length; i++)
+            {
+
+                hash += Content[i].Count(n => n == '#');
+
+            }
+
+            return hash;
+        }
+
+        public void ReplaceEdit(string file)
+        {
+            ReadText(file);
+
+            int hCount = 0;
+
+            int seperate = GetHashSum() / 2;
+
+            int m = 0;
+
+
+            for (m = 0; m < Content.Length; m++)
+            {
+                hCount += Content[m].Count(h => h == '#');
+
+                Content[m] = Content[m].Replace("#", "<");
+
+                if (hCount == seperate)
+                {
+                    break;
+                }
+            }
+
+            hCount = 0;
+
+            for (int i = m; i < Content.Length; i++)
+            {
+                hCount += Content[i].Count(h => h == '#');
+
+                
+                Content[i] = Content[i].Replace("#", ">");
+
+                if (hCount == seperate)
+                {
+                    break;
+                }
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+namespace SIGMA_5_SECOND_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EditText edit = new EditText();
+            edit.ReplaceEdit("C:\\Users\\lenovo\\source\\repos\\SIGMA_5(SECOND)\\SIGMA_5(SECOND)\\textfile.txt");
+
+            edit.Print();
+
+        }
+
+
+       
+    }
+}
